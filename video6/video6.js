8d7b6dca3de966e976f23d1d3fec4964ad3aec08
unknown <banghanam02@gmail.com>
2023-06-20 00:24:35 +0700

    video6

---
+++ video6/video6.js
@@ -0,0 +1,9 @@
+console.log(`hello wold from HTML`)
+let a =5; 
+let b=6;
+let c=6.9;
+let d='5'
+console.log(a+b)
+let e= +d;
+console.log('e value: ',e, 'type:', typeof e)
+console.log(a===e,'a: ',typeof a, 'd:',typeof d)
